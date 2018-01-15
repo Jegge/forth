81a34dff2451201a21c96d7ef1791723bcae762b
Sebastian Boettcher <jegge@gmx.net>
2018-01-15 07:58:09 +0100

    Initial Commit

---
+++ Forth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Forth
+//
+//  Created by Sebastian Boettcher on 15.01.18.
+//  Copyright © 2018 Sebastian Boettcher. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
