dcfadb7b0f97c254047fa8f4af9b099fc3bc95da
Sebastian Boettcher <jegge@gmx.net>
2018-01-26 12:51:50 +0100

    * Added projecteuler.net problem 3

--- Examples/peuler.f
+++ Examples/peuler.f
@@ -1,9 +1,10 @@
-
+\-------------------------------------------------------------------------------------------
+\
 \ https://projecteuler.net/problem=1
+\
 
 : IS-MULTIPLE   ( n1 n2 -- 0/1 if n1 is multiple of n2 )
-    MOD
-    NOT
+    MOD NOT
 ;
 
 : PEULER-001 ( limit -- result )
@@ -18,8 +19,9 @@
 ;
 
 \-------------------------------------------------------------------------------------------
-
+\
 \ https://projecteuler.net/problem=2
+\
 
 : IS-EVEN       ( n1 -- 0/1 )
     2 MOD NOT
@@ -48,5 +50,75 @@
     NIP                 ( sum )
 ;
 
+\-------------------------------------------------------------------------------------------
+\
+\ https://projecteuler.net/problem=3
+\
 
+: IS-EVEN       ( n1 -- 0/1 )
+    2 MOD NOT
+;
+
+: IS-MULTIPLE   ( n1 n2 -- 0/1 ) \ n1 is a multiple of n2
+    MOD NOT
+;
+
+: IS-PRIME     ( n -- 0/1 )
+    DUP 2 = IF
+        DROP                    (   )
+        TRUE                    ( result )
+        EXIT
+    THEN
+    DUP IS-EVEN IF
+        DROP                    (   )
+        FALSE                   ( result )
+        EXIT
+    THEN                        ( n )
+    3                           ( n index )
+    BEGIN
+        2DUP                    ( n index n index )
+        SWAP                    ( n index index n )
+        2 /                     ( n index index limit )
+        <                       \ while index < limit
+    WHILE                       ( n index )
+        2DUP IS-MULTIPLE IF     \ if n is a multiple of index
+            2DROP               (   )
+            FALSE               ( result )
+            EXIT
+        THEN
+        1+ 1+                   ( n index )
+    REPEAT
+    2DROP                       (   )
+    TRUE                        ( result )
+;
+
+: NEXT-PRIME-FACTOR ( n, factor -- n, factor )
+    BEGIN
+        2DUP                    ( n factor n factor )
+        SWAP                    ( n factor factor n )
+        2 /                     ( n factor factor limit )
+        <                       \ while factor < limit
+    WHILE                       ( n factor )
+        2DUP IS-MULTIPLE IF     \ if n is a multiple of factor
+            DUP IS-PRIME IF     \ if factor is a prime
+                TUCK            ( factor n factor )
+                /               ( factor n )
+                SWAP            ( n factor )
+                EXIT
+            THEN
+        THEN
+        1+
+    REPEAT                      ( factor n )
+    1-
+;
+
+: PEULER-003 ( n -- factor )
+    2                           ( n factor )
+    BEGIN
+        OVER IS-PRIME NOT       \ while n is not prime
+    WHILE
+        NEXT-PRIME-FACTOR       ( n factor )
+    REPEAT
+    DROP                        ( n )
+;
 
