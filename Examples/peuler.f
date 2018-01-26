63457f33789733dbb4bd6b38b7abad62fa2ce685
Sebastian Boettcher <jegge@gmx.net>
2018-01-26 10:42:58 +0100

    * Added projecteuler.net problem 2

--- Examples/peuler.f
+++ Examples/peuler.f
@@ -11,8 +11,8 @@
     DO
         I 3 IS-MULTIPLE ( sum r1 )
         I 5 IS-MULTIPLE ( sum r1 r2 )
-        OR IF           ( sum r1&r2)
-            I +         ( sum+i)
+        OR IF           ( sum r1|r2 )
+            I +         ( sum )
         THEN
     LOOP
 ;
@@ -25,26 +25,27 @@
     2 MOD NOT
 ;
 
-: FIBONACCI ( n1 n2 -- n2 n1+n2 n1 )
-    2DUP    ( n1 n2 n1 n2 )
-    +       ( n1 n2 n1+n2 )
-    ROT     ( n2 n1+n2 n1 )
+: FIBONACCI ( n1 n2 -- n2 n1+n2 )
+    TUCK    ( n2 n1 n2 )
+    +       ( n2 n1+n2 )
 ;
 
-: PEULER-002
-    0                   ( sum )
-    1 1                 ( sum n1 n2 )
+: PEULER-002    ( limit -- result )
+    0 1 1               ( limit sum n1 n2 )
     BEGIN
-        FIBONACCI       ( sum n1 n2 res )
-        DUP IS-EVEN IF
-            2SWAP       ( n2 res sum n1 )
-            -ROT        ( n2 n1 res sum )
-            +           ( n2 n1 sum )
-            ROT         ( sum n2 n1 )
-            SWAP        ( sum n1 n2 )
+        DUP 4 PICK <    \ while n2 < limit
+    WHILE               ( limit sum n1 n2 )
+        FIBONACCI       ( limit sum n1 n2 )
+        OVER IS-EVEN IF \ if n1 is even...
+            OVER        ( limit sum n1 n2 n1 )
+            2SWAP       ( limit n2 n1 sum n1 )
+            +           ( limit n2 n1 sum )
+            -ROT        ( limit sum n2 n1 )
+            SWAP        ( limit sum n1 n2 )
         THEN
-        DUP 100 >
-    UNTIL
+    REPEAT
+    2DROP               ( limit sum )
+    NIP                 ( sum )
 ;
 
 
